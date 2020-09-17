b61825394c141c9c1519ae6cb756712c9e8ddb14
agrarian-iot <eric.kimani@techcloud.co.ke>
2020-09-17 21:50:57 +0300

    Updated publish.yml

--- publish.ps1
+++ publish.ps1
@@ -1,38 +1,61 @@
 
 function updatePackages(){
-    apt update ;
-    apt install git -y;
+    apt-get update;
+    apt-get install git -y;
 }
 
 function pullSource(){
+    $source_path=$(Join-Path $env:PSModulePath.split(':')[2] $env:INPUT_PAYLOADNAME.Insert(0, '/'));
+
     git clone $env:INPUT_SOURCE;
+    
     cd $env:INPUT_SOURCE.split('/')[4].replace('.git', '');
-    cp ./*.ps* $(echo $env:PSModulePath).split(':')[2]
+    
+    mkdir $source_path;
+    
+    cp ./*.ps* $source_path
+}
+
+function getSourceVersion(){
+    $source_path=$(Join-Path $env:PSModulePath.split(':')[2] $env:INPUT_PAYLOADNAME.Insert(0, '/'));
+    
+    cd $source_path;
+
+    if($(Get-Variable $env:INPUT_PAYLOADREQUIREDVERSION).IsValidValue){
+
+        $env:INPUT_PAYLOADREQUIREDVERSION = $env:INPUT_PAYLOADREQUIREDVERSION
+    }
+    else 
+    {
+        # Set version to the next patch version number
+
+        $version = $(cat ./*.psd1 | select-string "ModuleVersion").Line.split(' = ')[1];
+        $version = $version.substring(1, $($version.length - 1));
+        $env:INPUT_PAYLOADREQUIREDVERSION = $(
+            $patch = $([Int]$version.split('.')[$($version.split('.').length - 1)] + 1);
+            $version.remove($version.length - 1).insert($($version.length - 1), $patch.ToString())
+        );
+    }
+    
+    
 }
 
 function publishSource(){
     # Update packages
     updatePackages
     
-    # # Pull Sources
-    # pullSource
+    # Pull Sources
+    pullSource
 
-    # # Check payload type script/module
-    # if($env:INPUT_PAYLOADTYPE -eq 'module'){
-    #     Publish-Module -Name $env:INPUT_PAYLOADNAME -NuGetApi $env:INPUT_NUGETAPIKEY -Repository $env:INPUT_REPOSITORY -RequiredVersion -Force
-    # }
-    # else
-    # {
-    #     Publish-Script -Name $env:INPUT_PAYLOADNAME -NuGetApi $env:INPUT_NUGETAPIKEY -Repository $env:INPUT_REPOSITORY -Force
-    # } 
+    # Check payload type script/module
+    if($env:INPUT_PAYLOADTYPE -eq 'module'){
+        Publish-Module -Name $env:INPUT_PAYLOADNAME -NuGetApi $env:INPUT_NUGETAPIKEY -Repository $env:INPUT_REPOSITORY -RequiredVersion $env:INPUT_PAYLOADREQUIREDVERSION -Force
+    }
+    else
+    {
+        Publish-Script -Name $env:INPUT_PAYLOADNAME -NuGetApi $env:INPUT_NUGETAPIKEY -Repository $env:INPUT_REPOSITORY -Force
+    } 
 
-    git version
-
-    echo "API_KEY :"$env:INPUT_NUGETAPIKEY;
-    echo "PAYLOAD_NAME :"$env:INPUT_PAYLOADNAME;
-    echo "PAYLOAD_TYPE :"$env:INPUT_PAYLOADTYPE;
-    echo "REPOSITORY :"$env:INPUT_REPOSITORY;
-    echo "INPUT_SOURCE :"$env:INPUT_SOURCE;
     
 }
 
